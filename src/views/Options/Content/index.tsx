fc8e3cb9009bd9339d220bdf939b8334ae8b209c
b9891796435 <76828507+b9891796435@users.noreply.github.com>
2022-03-31 10:23:32 +0800

    init Content

---
+++ src/views/Options/Content/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+const styles={
+    
+}
+class Content extends React.Component{
+    render(): React.ReactNode {
+        return(
+            <div>
+                <p>
+                </p>
+            </div>
+        )
+    }
+} 
+export default Content;
