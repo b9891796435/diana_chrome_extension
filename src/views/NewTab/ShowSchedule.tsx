e3419519c74ce70cc5bce340cc05a762646aa0d8
b9891796435 <76828507+b9891796435@users.noreply.github.com>
2023-03-04 13:04:59 +0800

    update schedule style

--- src/views/NewTab/ShowSchedule.tsx
+++ src/views/NewTab/ShowSchedule.tsx
@@ -8,7 +8,8 @@ import "./ShowSchedule.css";
 type scheduleState = {
     scheduleVisible: boolean,
     scheduleNow: scheduleType,
-    isGettingSchedule: boolean
+    isGettingSchedule: boolean,
+    fullScheduleDisplayed: boolean,
 }
 export default class ShowSchedule extends React.Component<{}, scheduleState>{
     constructor(props: any) {
@@ -17,6 +18,7 @@ export default class ShowSchedule extends React.Component<{}, scheduleState>{
             scheduleVisible: false,
             scheduleNow: 0,
             isGettingSchedule: false,
+            fullScheduleDisplayed: false,
         }
     }
     async componentDidMount() {
@@ -44,23 +46,20 @@ export default class ShowSchedule extends React.Component<{}, scheduleState>{
         ])
         this.setState({ isGettingSchedule: false });
     }
-    ImageRender = () => {//麻了，这就是屎山吗
-        if (typeof (this.state.scheduleNow) !== "number") {
+    ImageRender = (props: { schedule2Display?: any }) => {//麻了，这就是屎山吗
+        if (typeof (this.state.scheduleNow) !== "number" && typeof (props.schedule2Display) === 'number') {
             let nodeArray: JSX.Element[] = []
-            for (let i of this.state.scheduleNow.images) {
-                nodeArray.push(<img src={i.img_src} style={{ width: "100%" }} onClick={() => window.open(i.img_src)}></img>)
-            }
-            let jumpToDynamic = () => {
-                if (typeof this.state.scheduleNow !== 'number') {
-                    window.open('https://t.bilibili.com/' + this.state.scheduleNow.dynamicID)
+            if (this.state.fullScheduleDisplayed) {
+                for (let i of this.state.scheduleNow.images) {
+                    nodeArray.push(<img src={i.img_src} style={{ width: "100%" }} onClick={() => window.open(i.img_src)}></img>)
                 }
+            }
+            else {
+                let schedulePic = this.state.scheduleNow.images[props.schedule2Display];
+                nodeArray.push(<img src={schedulePic.img_src} style={{ width: "100%" }} onClick={() => window.open(schedulePic.img_src)}></img>)
             }
             return (
                 <div>
-                    <div>
-                        日程表共{this.state.scheduleNow.images.length}张,
-                        <span className={"linkClass"} onClick={jumpToDynamic}>跳转至日程表动态</span><br />
-                    </div>
                     {nodeArray}
                 </div>
             )
@@ -73,6 +72,11 @@ export default class ShowSchedule extends React.Component<{}, scheduleState>{
         const parseTime = (time: Date) => {
             return (time.getMonth() + 1) + "月" + time.getDate() + "日" + time.toString().split(" ")[4]
         }
+        let jumpToDynamic = () => {
+            if (typeof this.state.scheduleNow !== 'number') {
+                window.open('https://t.bilibili.com/' + this.state.scheduleNow.dynamicID)
+            }
+        }
         if (typeof (this.state.scheduleNow) === "number") {
             let getDate = new Date(this.state.scheduleNow);
             return (<div style={{ fontSize: "16px" }}>
@@ -82,10 +86,21 @@ export default class ShowSchedule extends React.Component<{}, scheduleState>{
         } else {
             let dynamicDate = new Date(this.state.scheduleNow.dynamicDate);
             let getDate = new Date(this.state.scheduleNow.getDate);
+            let timeGap = Math.floor((Date.now() - this.state.scheduleNow.getDate) / 86400000);
+            let week = Math.floor(timeGap / 7);
+            let day = timeGap % 7;
+            let schedule2Display = week;
+            if (day >= 5 && new Date().getDay() <= 2) {
+                schedule2Display++;
+            }
             return (<div style={{ fontSize: "16px" }}>
                 日程表发布时间：{parseTime(dynamicDate)}<br />
-                日程表抓取时间：{parseTime(getDate)}，<span className="linkClass" onClick={this.getLiveStateWrapper}>点击重试</span><br />
-                <ImageRender />
+                日程表抓取时间：{parseTime(getDate)}<br />
+                日程表共{this.state.scheduleNow.images.length}张{(() => { if (!this.state.fullScheduleDisplayed) return '，当前显示第' + (schedule2Display + 1) + '张' })()}<br />
+                <MyButton text="重试抓取" onClick={this.getLiveStateWrapper}></MyButton>
+                <MyButton text="跳转至日程表动态" onClick={jumpToDynamic}></MyButton>
+                <MyButton text="显示完整日程表" onClick={() => this.setState({ fullScheduleDisplayed: true })}></MyButton>
+                <ImageRender schedule2Display={schedule2Display} />
             </div>)
         }
     }
